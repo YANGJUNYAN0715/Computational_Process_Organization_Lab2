38872c2f00502a074e3fb693e236390bf5f4a126
Junyan Yang <1310204834@qq.com>
2022-04-18 17:17:19 +0800

    Third commit

--- unrolled_linked_list_test.py
+++ unrolled_linked_list_test.py
@@ -5,117 +5,117 @@ from unrolled_linked_list import UnrolledLinkedList
 
 class unrolled_linked_list_test(unittest.TestCase):
     def test_default_node_capacity(self):
-        l = UnrolledLinkedList()
-        self.assertEqual(l.max_node_capacity, 4)
+        L = UnrolledLinkedList()
+        self.assertEqual(L.max_node_capacity, 4)
 
     def test_custom_node_capacity(self):
-        l = UnrolledLinkedList(4)
-        self.assertEqual(l.max_node_capacity, 4)
+        L = UnrolledLinkedList(4)
+        self.assertEqual(L.max_node_capacity, 4)
 
-        l = UnrolledLinkedList(15)
-        self.assertEqual(l.max_node_capacity, 15)
+        L = UnrolledLinkedList(15)
+        self.assertEqual(L.max_node_capacity, 15)
 
     def test_empty(self):
-        l = UnrolledLinkedList()
-        self.assertEqual(str(l), "{}")
-        l.append(1)
-        del l[0]
-        self.assertEqual(str(l), "{}")
+        L = UnrolledLinkedList()
+        self.assertEqual(str(L), "{}")
+        L.append(1)
+        del L[0]
+        self.assertEqual(str(L), "{}")
 
     def test_delete_item(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
-        del l[1]
-        self.assertEqual(str(l), '{[1, 3]}')
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
+        del L[1]
+        self.assertEqual(str(L), '{[1, 3]}')
 
     def test_get_item(self):
-        l = UnrolledLinkedList()
-        l.append(2)
-        l.append(3)
-        self.assertEqual(l.__getitem__(1), 3)
-        self.assertEqual(str(l[0]), '2')
+        L = UnrolledLinkedList()
+        L.append(2)
+        L.append(3)
+        self.assertEqual(L.__getitem__(1), 3)
+        self.assertEqual(str(L[0]), '2')
 
     def test_set_item(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
-        l[1] = 42
-        self.assertEqual(str(l[1]), '42')
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
+        L[1] = 42
+        self.assertEqual(str(L[1]), '42')
 
     def test_iteration(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
         arr = []
-        for i in l:
+        for i in L:
             arr.append(i)
 
-        self.assertEqual(arr[0], l[0])
-        self.assertEqual(arr[1], l[1])
-        self.assertEqual(arr[2], l[2])
+        self.assertEqual(arr[0], L[0])
+        self.assertEqual(arr[1], L[1])
+        self.assertEqual(arr[2], L[2])
 
     def test_len(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
 
-        self.assertEqual(str(len(l)), '3')
+        self.assertEqual(str(len(L)), '3')
 
-        l.append(4)
+        L.append(4)
 
-        self.assertEqual(str(len(l)), '4')
+        self.assertEqual(str(len(L)), '4')
 
     def test_reverse(self):
         testL = [3, 2, 1]
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
 
         newL = []
 
-        for x in reversed(l):
+        for x in reversed(L):
             newL.append(x)
         self.assertEqual(newL, testL)
 
     def test_contains(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
-        l.append(4)
-        l.append(5)
-        l.append(6)
-        l.append(7)
-        l.append(8)
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
+        L.append(4)
+        L.append(5)
+        L.append(6)
+        L.append(7)
+        L.append(8)
 
-        self.assertEqual(1 in l, True)
-        self.assertEqual(42 in l, False)
-        self.assertEqual(8 in l,True)
+        self.assertEqual(1 in L, True)
+        self.assertEqual(42 in L, False)
+        self.assertEqual(8 in L, True)
 
     def test_variable(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(3)
-        l.append(4)
-        l.append(5)
-        l.append(1)
-        l.append(7)
-        l.append(8)
-        self.assertEqual(str(l), '{[1, 3], [4, 5], [1, 7, 8]}')
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(3)
+        L.append(4)
+        L.append(5)
+        L.append(1)
+        L.append(7)
+        L.append(8)
+        self.assertEqual(str(L), '{[1, 3], [4, 5], [1, 7, 8]}')
 
     def test_to_list(self):
-        l = UnrolledLinkedList()
-        l.append(1)
-        l.append(2)
-        l.append(3)
-        l.append(4)
-        self.assertEqual(str(l.to_list()),'[1, 2, 3, 4]')
+        L = UnrolledLinkedList()
+        L.append(1)
+        L.append(2)
+        L.append(3)
+        L.append(4)
+        self.assertEqual(str(L.to_list()), '[1, 2, 3, 4]')
 
     # def test_from_list(self):
     #     l = UnrolledLinkedList()
