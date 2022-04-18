7c7f95375b5d4961e80d42c3e47f289ed5fa9500
YANGJUNYAN0715 <85329582+YANGJUNYAN0715@users.noreply.github.com>
2022-04-18 16:37:05 +0800

    Update unrolled_linked_list.py

--- unrolled_linked_list.py
+++ unrolled_linked_list.py
@@ -40,7 +40,7 @@ class UnrolledLinkedList():
                 numberToTransfer = self.max_node_capacity // 2 \
                                    - len(currentNode.arr) + 1
                 currentNode.arr = currentNode.arr + \
-                                  (nextNode.arr[:numberToTransfer])
+                                  nextNode.arr[:numberToTransfer]
                 nextNode.arr = nextNode.arr[numberToTransfer:]
 
                 if len(nextNode.arr) < self.max_node_capacity // 2:
